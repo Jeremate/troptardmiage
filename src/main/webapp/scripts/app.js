f3b5ccb69c1d5557aefa2262358bd54b4b37e60a
Jeremate <jeremate@gmail.com>
2015-06-05 14:47:40 +0200

    modification des éléments pour deploy

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -1,37 +1,37 @@
 'use strict';
 
 var ttmApp = angular.module('troptardmiage', [
-	'ui.bootstrap', 'ui.router'
-	]);
+    'ui.bootstrap', 'ui.router'
+]);
 
 ttmApp.config(function ($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('main');
+    $urlRouterProvider.otherwise('main');
 
-	$stateProvider
-		.state('main', {
-			url: '/',
-			views: {
-				'': {
-					templateUrl: 'views/main.html'
-				},
-				'navbar-main@': {
-					templateUrl: 'views/navbarmain.html'
-				}
-			}
-		})
+    $stateProvider
+    .state('main', {
+       url: '/',
+       views: {
+         '': {
+            templateUrl: 'views/main.html'
+         },
+         'navbar-main@': {
+            templateUrl: 'views/navbarmain.html'
+         }
+       }
+    })
 
-		.state('events', {
-			url: '/events',
-			views: {
-				'': { 
-					templateUrl: 'views/events.html' 
-				},
-				'navbar-main@': {
-					templateUrl: 'views/navbarmain.html'
-				},
-				'navbar-user@events': {
-					templateUrl: 'views/navbaruser.html'
-				}
-			}
-		});
+    .state('events', {
+       url: '/events',
+       views: {
+            '': { 
+                templateUrl: 'views/events.html' 
+            },
+            'navbar-main@': {
+                templateUrl: 'views/navbarmain.html'
+            },
+            'navbar-user@events': {
+                templateUrl: 'views/navbaruser.html'
+            }
+        }
+    });
 });
